ee21e84b6f2650ee3e7174efbf45cec4f3a00682
reaper <reaper>
2023-03-26 14:12:04 +0300

    Выполнено

---
+++ classes/__init__.py
@@ -0,0 +1,10 @@
+from .wild.wolf import Wolf
+from .wild.tiger import Tiger
+from classes.home.home import Home
+from .home.cat import Cat
+from .home.dog import Dog
+from .bird.hen import Hen
+from .bird.stork import Stork
+
+from .interfaces.dogTraining import DogTraining
+from .interfaces.birdFly import BirdFly
